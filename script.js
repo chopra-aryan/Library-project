9626c491d36a6c3a3865e789a7ef9e4788c77433
Aryan Chopra <209628710+chopra-aryan@users.noreply.github.com>
2025-07-12 00:13:07 +0530

    final neat and simle code, tried to keep it as raw as possible, but neat at the same time

--- script.js
+++ script.js
@@ -11,24 +11,38 @@ function Book(title, author, pages, isRead) {
 const book1 = new Book('ABC', 'Who cares?', 420, true);
 const book2 = new Book('XYZ', 'no one gives a f***', 69, false);
 
-
-
-
-function addBookToLibrary() {
-    
-}
-
 myLibrary.push(book1, book2);
+
+
+
+function addBookToLibrary(event) {
+    event.preventDefault();
+    const title = event.target.title.value;
+    const author = event.target.author.value;
+    const pages = event.target.pages.value;
+    const readStatus= event.target.readStatus.checked;
+
+    const newBook = new Book(title, author, pages, readStatus);
+    myLibrary.push(newBook);  
+
+    event.target.reset();
+    displayBooks();
+    closeModal();
+}
 
 const libraryContainer = document.querySelector('.library-container');
 
+function closeModal() {
+    document.querySelector('.modal').style.display = 'none';
+}
 
 function showForm() {
     document.querySelector('.modal').style.display = 'flex';
 }
 
 function displayBooks() {
-    const bookTable = document.querySelector('table');
+    const bookTable = document.querySelector('table tbody');
+    bookTable.innerHTML ="";
     myLibrary.forEach(book => {
         
         const tableRow = document.createElement('tr');
@@ -45,13 +59,22 @@ function displayBooks() {
         const read = document.createElement('td');
 
         const span = document.createElement('span');
-        span.textContent = book.isRead ? 'yes' : 'no';
+        span.textContent = book.isRead ? 'Read' : 'Not Read';
+
+        
 
         const checkBox = document.createElement('input');
         checkBox.type = 'checkbox';
 
-        checkBox.checked = span.textContent === 'yes';
-        checkBox.onchange = () => {span.textContent =  checkBox.checked ? 'yes':  'no'}
+        checkBox.checked = span.textContent === 'Read';
+
+        checkBox.style.accentColor = 'green';
+
+        span.style.color = checkBox.checked ? 'green': 'red';
+
+        checkBox.onchange = () => {span.textContent =  checkBox.checked ? 'Read':  'Not Read';
+            span.style.color = checkBox.checked ? 'green': 'red';
+        }
 
         read.appendChild(span);
         read.appendChild(checkBox);
@@ -61,6 +84,14 @@ function displayBooks() {
         itag.classList.add('fas','fa-trash');
         itag.style.cursor = 'pointer';
         
+        itag.onclick = () => {
+            const i = myLibrary.findIndex((libraryBook) => libraryBook.id === book.id);
+            if (i !== -1) {
+                myLibrary.splice(i, 1);
+                displayBooks();
+            } 
+        }
+
         delIcon.appendChild(itag);
 
         tableRow.appendChild(title);
